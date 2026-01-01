9b07a91d3396d14d26ad986913b4810f8f622cf3
hp <ajsjajay@gmail.com>
2026-01-01 14:56:48 +0000

    Guihub push Customer data

--- Data base and file creation Collection.py
+++ Data base and file creation Collection.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://username:<Password>@Clustername.53vs8nv.mongodb.net/?appName=Ineuronprojects"
+uri = "Your URL MONGO DB to be pasetd"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
